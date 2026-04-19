c47442d7cb26db3b1d8fa8e12b780855491c447c
puloop <elpayaso.v@gmail.com>
2026-04-18 22:01:43 -0400

    reto 1 y creacion del reto dos en proceso

---
+++ Reto1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO; 
+
+class Program
+{
+    static void Main()
+    {
+        
+        string registro = "usuario; ClaveSegura123";
+
+       
+        string[] partes = registro.Split(';');
+        string usuario = partes[0].Trim();
+        string clave = partes[1].Trim();
+
+        
+        if (clave.Contains("123"))
+        {
+            //
+            // Usamos el bloque 'using' para devolver el archivo al sistema
+            using (StreamWriter sw = new StreamWriter("seguridad.txt", true))
+            {
+                sw.WriteLine("Atencion: Clave Debil detectada para: " + usuario);
+            }
+            Console.WriteLine("-->Registro guardado exitosamente en seguridad.txt");
+        }
+
+        Console.WriteLine("====== PROCESO FINALIZADO ======");
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
